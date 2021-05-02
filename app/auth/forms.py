5507b036fc264b1668a3cc760bf1d59c947708e8
Marya <maryammuchai@gmail.com>
2021-05-02 13:40:00 +0300

    creating the registration form

---
+++ app/auth/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField,SubmitField,BooleanField
+from wtforms.validators import Required, Email
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',validators=[Required()])
+    password = PasswordField('Password',validators=[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')
+    
+class RegForm(FlaskForm):
+    username = StringField('Enter your Username',validators=[Required()])
+    email = StringField('Email',validators=[Required(),email()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Password must match')])
+    password_confirm = PasswordField('Confirm Password',validators = [Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError(message="This email has been taken")
+
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValiadtionError(message="This username has already been taken")
