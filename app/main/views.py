6452565b30fc76c7604925cc4b1ac7d601b9f406
Marya <maryammuchai@gmail.com>
2021-05-02 14:30:43 +0300

    registering the blueprint instance

---
+++ app/main/views.py
@@ -0,0 +1,6 @@
+import os
+from flask import render_template,redirect,url_for,abort,request,flash
+from app.main import main
+from .forms import UpdateProfile,NewBlog
+from flask_login import login_required,current_user
+
