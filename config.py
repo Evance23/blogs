761718aca8fc337edfdf5bded1b9b57f7ceb5549
Marya <maryammuchai@gmail.com>
2021-05-05 22:49:43 +0300

    updating the secret key

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ class Config:
     QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maryam:1234@localhost/blog'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -22,7 +21,7 @@ class Config:
         pass
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres://mqtrcwkoyajdls:d9140b4145e49978aab6a5642c8ab79074c41af39a5955a362108b1777ba5e05@ec2-52-21-252-142.compute-1.amazonaws.com:5432/dfdj7mklrqk41n?sslmode=require"
+    SQLALCHEMY_DATABASE_URI = "postgresql://mqtrcwkoyajdls:d9140b4145e49978aab6a5642c8ab79074c41af39a5955a362108b1777ba5e05@ec2-52-21-252-142.compute-1.amazonaws.com:5432/dfdj7mklrqk41n?sslmode=require"
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maryam:1234@localhost/blog'
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export SECRET_KEY='f67890-0091-yhgh5-fujlj-2342345'
+export SECRET_KEY='6cacec3e-adda-11eb-ac94-b7f8cfddb685'
 export MAIL_USERNAME='maryammuchai@gmail.com'
 export MAIL_PASSWORD='Maryam2020.'
 python3 manage.py server 
